c3fb58547a0304e2f030914c147f2844412e82ba
sanlokaja <sandyloka790@gmail.com>
2022-12-15 15:59:43 +0700

    docs: add geopardy information card components

---
+++ src/components/home/GeopardyInformationCard.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Flex, Text, Heading, Image } from "@chakra-ui/react";
+
+const GeopardyInformationCard = ({ icon, title, bgColor, paragraph }) => {
+
+    return (
+        <Flex
+            flexDirection="column"
+            minW="257px"
+            bg="white"
+            borderRadius="10px"
+            border="1px solid #BFBFBF"
+            justifyContent="space-between"
+            alignItems="center"
+            gap="44px"
+            zIndex="100"
+        >
+            <Flex
+                flexDirection="column"
+                maxH="300px"
+                maxW="277px"
+                gap="20px"
+                justifyContent="center"
+                alignItems="center"
+                textAlign="center"
+                mt="25px"
+            >
+                <Flex
+                    borderRadius="full"
+                    backgroundColor={bgColor}
+                    boxSize="50px"
+                    justifyContent="center"
+                    alignItems="center"
+                >
+                    <Image maxW="42px" src={icon} alt={title} />
+                </Flex>
+                <Heading fontSize="22px" fontWeight="700" fontFamily="Poppins" color="#323232">
+                    {title}
+                </Heading>
+                <Text fontSize="20px" fontWeight="500" color="#323232" fontFamily="Poppins">
+                    {paragraph}
+                </Text>
+            </Flex>
+        </Flex>
+    );
+};
+
+export default GeopardyInformationCard;
