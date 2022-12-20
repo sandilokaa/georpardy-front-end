0f86d2825692688ec8c808e89eecf27267ef7564
sanlokaja <sandyloka790@gmail.com>
2022-12-20 15:39:51 +0700

    docs: add router area distribution

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import Home from "../pages/home/Home"
+import Home from "../pages/home/Home";
+import AreaDistribution from "../pages/area-distribution/AreaDistribution";
 
 const Router = () => {
     return (
         <Routes>
 
             <Route path="/" element={<Home />}></Route>
+            <Route path="/area-distribution" element={<AreaDistribution />}></Route>
 
         </Routes>
     );
